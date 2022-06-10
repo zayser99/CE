02c0d31905cf1a503ca0bcf3aa921d291a09c807
PanchoPistolaz <AlanCrvantesPrez1598@hotmail.com>
2022-06-10 12:14:42 -0500

    Pobladores de facultad y carrera

--- poblador.sql
+++ poblador.sql
@@ -1,1 +1,47 @@
 INSERT INTO `tipocarrera` (`tipoca_id`, `tipoca_nombre`) VALUES (NULL, 'LICENCIATURA'), (NULL, 'MAESTRIA '), (NULL, 'BACHILLERATO');
+
+-- Poblador facultad
+INSERT INTO `facultad` (`facultad_id`, `facultad_nombre`) VALUES (NULL,`DERECHO`), (NULL, `CIENCIAS ECONÓMICAS ADMINISTRATIVAS`), (NULL `CIENCIAS EDUCATIVAS`), (NULL `QUIMICA`), (NULL `CIENCIAS DE LA INFORMACION`), (NULL `INGENIERIA`), (NULL `MEDICINA`), (NULL `CIENCIAS NATURALES EXACTAS`);
+
+INSERT INTO `facultad` (`facultad_id`, `facultad_nombre`) VALUES  (NULL, `DERECHO`);
+INSERT INTO `facultad` (`facultad_id`, `facultad_nombre`) VALUES  (NULL `MEDICINA`);
+INSERT INTO `facultad` (`facultad_id`, `facultad_nombre`) VALUES  (NULL `CIENCIAS ECONOMICAS ADMINISTRATIVAS`);
+INSERT INTO `facultad` (`facultad_id`, `facultad_nombre`) VALUES  (NULL `CIENCIAS DE LA INFORMACION`);
+INSERT INTO `facultad` (`facultad_id`, `facultad_nombre`) VALUES  (NULL `CIENCIAS EDUCATIVAS`);
+INSERT INTO `facultad` (`facultad_id`, `facultad_nombre`) VALUES  (NULL, `QUIMICA`);
+INSERT INTO `facultad` (`facultad_id`, `facultad_nombre`) VALUES  (NULL, `INGENIERIA`);
+INSERT INTO `facultad` (`facultad_id`, `facultad_nombre`) VALUES  (NULL, `CIENCIAS NATURALES EXACTAS`);
+
+
+-- poblador carrera
+
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `CRIMINOLOGIA Y CRIMINALISTICA`), (NULL `1`), (NULL `1`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `DERECHO`), (NULL `1`), (NULL `1`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `BIOLOGIA MARINA`), (NULL `1`), (NULL `8`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `EDUCACION`), (NULL `1`), (NULL `4`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `LENGUA INGLESA`), (NULL `1`), (NULL `4`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `COMUNICACION Y GESTION CULTURAL`), (NULL `1`), (NULL `4`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `INGENIERIA QUIMICA`), (NULL `1`), (NULL `5`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `INGENIERIA PETROLERA`), (NULL `1`), (NULL `5`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `INGENIERIA GEOLOGICA`), (NULL `1`), (NULL `5`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `INGENIERIA EN SISTEMA COMPUTACIONALES`), (NULL `1`), (NULL `2`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `DISEÑO MULTIMEDIA`), (NULL `1`), (NULL `2`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `TECNOLOGIAS DE COMPUTO Y COMUNICACIONES`), (NULL `1`), (NULL `2`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `ADMINISTRACION DE EMPRESAS`), (NULL `1`), (NULL `3`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `CONTADUIRIA`), (NULL `1`), (NULL `3`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `ADMINISTRACION TURISTICA`), (NULL `1`), (NULL `3`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `MERCADOTECNIA`), (NULL `1`), (NULL `3`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `NEGOCIOS INTERNACIONALES`), (NULL `1`), (NULL `3`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `INGENIERIA MECATRONICA`), (NULL `1`), (NULL `7`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `INGENIERIA CIVIL`), (NULL `1`), (NULL `7`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `INGENIERIA MECANICA`), (NULL `1`), (NULL `7`);
+
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `INGENIERIA GEOFISICA`), (NULL `1`), (NULL `7`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `INGENIERIA EN ENERGIA`), (NULL `1`), (NULL `7`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `ARQUITECTURA SUSTENTABLE`), (NULL `1`), (NULL `7`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `MEDICINA`), (NULL `1`), (NULL `6`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `PSICOLOGIA`), (NULL `1`), (NULL `6`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `FISIOTERAPIA`), (NULL `1`), (NULL `6`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `ENFERMERIA`), (NULL `1`), (NULL `6`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `NUTRICION`), (NULL `1`), (NULL `6`);
+INSERT INTO `carrera` (`carrea_id`, `carrea_nombre,`, `tipoca_id`, `facultad_id`) VALUES  (NULL, `EDUCACION FISICA Y DEPORTE`), (NULL `1`), (NULL `6`);
