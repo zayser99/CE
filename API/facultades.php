1770fdf495aad8546bf94e1d1b2e86a0b560c696
ZAYSER99 <zayser99@hotmail.com>
2022-06-16 17:29:21 -0500

    se comento el codigo

--- API/facultades.php
+++ API/facultades.php
@@ -2,40 +2,40 @@
 include_once 'conexion.php';
 $objeto = new conexion();
 $conexion = $objeto->Conectar();
-
+//recibimos por post el nombre de la facultas
 $facultad_nombre = (isset($_POST['facultad_nombre'])) ? $_POST['facultad_nombre'] : '';
 
-
+//recibimos la opcion (eliminar, modificar, agregar) y en dado caso que se ocupe la id para editar el registro 
 $opcion = (isset($_POST['opcion'])) ? $_POST['opcion'] : '';
 $facultad_id = (isset($_POST['facultad_id'])) ? $_POST['facultad_id'] : '';
 
 
 
 switch ($opcion) {
-    case 1:
-        $consulta = "INSERT INTO facultad (facultad_id, facultad_nombre) VALUES(null, '$facultad_nombre') ";
-        $resultado = $conexion->prepare($consulta);
+    case 1: //agregar
+        $consulta = "INSERT INTO facultad (facultad_id, facultad_nombre) VALUES(null, '$facultad_nombre') ";   //genramos el insert
+        $resultado = $conexion->prepare($consulta); // lo ejeutamos
         $resultado->execute();
 
-        $consulta = "SELECT * FROM facultad ORDER BY facultad_nombre DESC LIMIT 1";
-        $resultado = $conexion->prepare($consulta);
+        $consulta = "SELECT * FROM facultad ORDER BY facultad_nombre DESC LIMIT 1"; //ultimo dato en el orden alfabetico
+        $resultado = $conexion->prepare($consulta);// la ejecutamos 
         $resultado->execute();
-        $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
+        $data = $resultado->fetchAll(PDO::FETCH_ASSOC);  // devolvemos la consulta
         break;
-    case 2:
-        $consulta = "UPDATE facultad SET facultad_nombre='$facultad_nombre' WHERE facultad_id='$facultad_id' ";
+    case 2://editar
+        $consulta = "UPDATE facultad SET facultad_nombre='$facultad_nombre' WHERE facultad_id='$facultad_id' "; //generamos el update
         $resultado = $conexion->prepare($consulta);
-        $resultado->execute();
+        $resultado->execute();//ejecutamos el update 
 
-        $consulta = "SELECT * FROM facultad WHERE facultad_id='$facultad_id' ";
+        $consulta = "SELECT * FROM facultad WHERE facultad_id='$facultad_id' ";  // consultamos el editato 
         $resultado = $conexion->prepare($consulta);
         $resultado->execute();
-        $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
+        $data = $resultado->fetchAll(PDO::FETCH_ASSOC);// devolvemos la consulta
         break;
-    case 3:
-        $consulta = "DELETE FROM facultad WHERE facultad_id='$facultad_id' ";
+    case 3://eliminar
+        $consulta = "DELETE FROM facultad WHERE facultad_id='$facultad_id' "; //generamos el delete
         $resultado = $conexion->prepare($consulta);
-        $resultado->execute();
+        $resultado->execute(); //ejecutamos el delete
         break;
 }
 
